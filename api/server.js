1366880cd9937864dee2f4c51ec70f2b27201da7
Shubham Mishra <shubhammishra10101998@gmail.com>
2023-06-28 12:33:17 +0530

    Initial Rest API for goals

--- api/server.js
+++ api/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const colors = require("colors")
 const dotenv = require("dotenv").config()
 const goalRoutes = require("./routes/goalRoutes");
 const { errorHandler } = require("./middleware/errorMiddleware")
+const connectDB = require("./config/db")
 const PORT = process.env.PORT
+
+connectDB()
 
 const app = express();
 
